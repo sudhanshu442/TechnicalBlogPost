d072f07c10d88cb7ec5902214005948d766cc696
Sudhanshu441 <sudhanshu1834.cse18@chitkara.edu.in>
2021-02-25 18:14:22 +0530

    byee

---
+++ src/main/java/com/upgrad/technicalpost/TechnicalpostApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.technicalpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upgrad/technicalpost/TechnicalpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.technicalpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
